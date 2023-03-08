87be22a0c569ce7e960a3aca5b590d9e62444dc2
Richard Hsu <45455105+bung8833@users.noreply.github.com>
2023-03-08 15:00:09 +0800

    bug ver commit

---
+++ prjMvcDemo/prjMvcDemo/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using prjMvcDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+
+namespace prjMvcDemo.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public ActionResult List()
+        {
+            List<Customer> customers = new CustomerFactory().QueryAll();
+            return View(customers);
+        }
+    }
+}
--- prjMvcDemo/prjMvcDemo/Models/CustomerFactory.cs
+++ prjMvcDemo/prjMvcDemo/Models/CustomerFactory.cs
@@ -4,14 +4,44 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Web;
+using System.Web.UI.WebControls;
 
 namespace prjMvcDemo.Models
 {
     public class CustomerFactory
     {
+        const string _tableName = "tCustomer";
+
+
+        public List<Customer> QueryAll()
+        {
+            string sql = $"SELECT * FROM {_tableName}";
+
+            List<Customer> customers = QueryBySql(sql, null);
+
+            return customers;
+        }
+
+
+        public Customer QueryById(int fId)
+        {
+            string sql = $"SELECT * FROM {_tableName} WHERE fId=@K_FID";
+
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            parameters.Add(new SqlParameter("K_FID", fId));
+
+            List<Customer> customers = QueryBySql(sql, parameters);
+            if (customers == null)
+            {
+                return null;
+            }
+            return customers.Single();
+        }
+
+
         public void Delete(int fId)
         {
-            string sql = "DELETE FROM tCustomer WHERE fId=@K_FID";
+            string sql = $"DELETE FROM {_tableName} WHERE fId=@K_FID";
 
             List<SqlParameter> parameters = new List<SqlParameter>();
             parameters.Add(new SqlParameter("K_FID",(object)fId));
@@ -20,10 +50,9 @@ namespace prjMvcDemo.Models
         }
 
 
-
         public void Create(Customer p)
         {
-            string sql = $@"INSERT INTO tCustomer (
+            string sql = $@"INSERT INTO {_tableName} (
 fName, fPhone, fAddress, fEmail, fPassword)
 VALUES(
 @fName, @fPhone, @fAddress, @fEmail, @fPassword
@@ -38,6 +67,11 @@ VALUES(
 
             ExecuteSql(sql, parameters);
         }
+
+
+        //----------------------------------------------------------//
+        //----------------------------------------------------------//
+        //----------------------------------------------------------//
 
 
         private void ExecuteSql(string sql, List<SqlParameter> parameters)
@@ -59,6 +93,63 @@ VALUES(
             }
         }
 
+        
+        private List<Customer> QueryBySql(string sql, List<SqlParameter> parameters)
+        {
+            List<Customer> customers = new List<Customer>();
+
+            using (var conn = new SqlConnection())
+            {
+                conn.ConnectionString = "Data Source=.;Initial Catalog=dbDemo;Integrated Security=True";
+
+                using (var cmd = new SqlCommand(sql, conn))
+                {
+                    if (parameters != null)
+                    {
+                        cmd.Parameters.AddRange(parameters.ToArray());
+                    }
+
+                    conn.Open();
+
+                    var reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Customer customer = new Customer();
+
+                        customer.fId = (int)reader["fId"];
+
+                        if (reader["fName"].Equals(DBNull.Value) == false)
+                        {
+                            customer.fName = reader["fName"].ToString();
+                        }
+
+                        if (reader["fPhone"].Equals(DBNull.Value) == false)
+                        {
+                            customer.fPhone = reader["fPhone"].ToString();
+                        }
+
+                        if (reader["fAddress"].Equals(DBNull.Value) == false)
+                        {
+                            customer.fAddress = reader["fAddress"].ToString();
+                        }
+
+                        if (reader["fEmail"].Equals(DBNull.Value) == false)
+                        {
+                            customer.fEmail = reader["fEmail"].ToString();
+                        }
+
+                        if (reader["fPassword"].Equals(DBNull.Value) == false)
+                        {
+                            customer.fPassword = reader["fPassword"].ToString();
+                        }
+
+                        customers.Add(customer);
+                    }
+                }
+            }
+
+            return customers;
+        }
 
         // end of class CustomerFactory
     }
