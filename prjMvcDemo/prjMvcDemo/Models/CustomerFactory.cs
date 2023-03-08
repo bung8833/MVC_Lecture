c6fdba4aa057337651af06ca7da60de08c6993b2
Richard Hsu <45455105+bung8833@users.noreply.github.com>
2023-03-08 10:37:22 +0800

    add ExecuteSql()

--- prjMvcDemo/prjMvcDemo/Models/CustomerFactory.cs
+++ prjMvcDemo/prjMvcDemo/Models/CustomerFactory.cs
@@ -9,24 +9,57 @@ namespace prjMvcDemo.Models
 {
     public class CustomerFactory
     {
+        public void Delete(int fId)
+        {
+            string sql = "DELETE FROM tCustomer WHERE fId=@K_FID";
+
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            parameters.Add(new SqlParameter("K_FID",(object)fId));
+
+            ExecuteSql(sql, parameters);
+        }
+
+
+
         public void Create(Customer p)
         {
             string sql = $@"INSERT INTO tCustomer (
 fName, fPhone, fAddress, fEmail, fPassword)
 VALUES(
-'{p.fName}', '{p.fPhone}', '{p.fAddress}', '{p.fEmail}', '{p.fPassword}'
+@fName, @fPhone, @fAddress, @fEmail, @fPassword
 )";
 
+            List<SqlParameter> parameters= new List<SqlParameter>();
+            parameters.Add(new SqlParameter("fName", p.fName));
+            parameters.Add(new SqlParameter("fPhone", p.fPhone));
+            parameters.Add(new SqlParameter("fAddress", p.fAddress));
+            parameters.Add(new SqlParameter("fEmail", p.fEmail));
+            parameters.Add(new SqlParameter("fPassword", p.fPassword));
+
+            ExecuteSql(sql, parameters);
+        }
+
+
+        private void ExecuteSql(string sql, List<SqlParameter> parameters)
+        {
             using (var conn = new SqlConnection())
             {
-                conn.ConnectionString = "Data Source=P215-2203-NB01;Initial Catalog=dbDemo;Integrated Security=True";
+                conn.ConnectionString = "Data Source=.;Initial Catalog=dbDemo;Integrated Security=True";
 
                 using (var cmd = new SqlCommand(sql, conn))
                 {
+                    if (parameters != null)
+                    {
+                        cmd.Parameters.AddRange(parameters.ToArray());
+                    }
+                    
                     conn.Open();
                     cmd.ExecuteNonQuery();
                 }
             }
         }
+
+
+        // end of class CustomerFactory
     }
 }
